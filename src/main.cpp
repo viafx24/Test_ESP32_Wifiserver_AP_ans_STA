67ec89d127cce2683f589ffb805bb0038efa31af
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-08 11:38:17 +0200

    WifiserveurSTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
 
-/*
- WiFi Web Server LED Blink
+// /*
+//  WiFi Web Server LED Blink
 
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 5.
+//  A simple web server that lets you blink an LED via the web.
+//  This sketch will print the IP address of your WiFi Shield (once connected)
+//  to the Serial monitor. From there, you can open that address in a web browser
+//  to turn on and off the LED on pin 5.
 
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
+//  If the IP address of your shield is yourAddress:
+//  http://yourAddress/H turns the LED on
+//  http://yourAddress/L turns it off
 
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
+//  This example is written for a network using WPA encryption. For
+//  WEP or WPA, change the Wifi.begin() call accordingly.
 
- Circuit:
- * WiFi shield attached
- * LED attached to pin 5
+//  Circuit:
+//  * WiFi shield attached
+//  * LED attached to pin 5
 
- created for arduino 25 Nov 2012
- by Tom Igoe
+//  created for arduino 25 Nov 2012
+//  by Tom Igoe
 
-ported for sparkfun esp32 
-31.01.2017 by Jan Hendrik Berlin
+// ported for sparkfun esp32 
+// 31.01.2017 by Jan Hendrik Berlin
  
- */
+//  */
 #include <Arduino.h>
 #include <WiFi.h>
 
